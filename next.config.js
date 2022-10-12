d48215f7030311b3ddc29a5b4e41a727404f21dd
Chilo Nwokolo <chilo.nwokolo@gmail.com>
2022-10-12 20:49:04 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	env: {
+		NEXT_PUBLIC_CONFIG_URL: process.env.NEXT_PUBLIC_URL,
+		PUBLIC_CONFIG_URL: process.env.PUBLIC_URL,
+	},
+};
 
 module.exports = nextConfig
